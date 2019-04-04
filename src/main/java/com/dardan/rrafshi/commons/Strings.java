90d9c63f45f41ca1c787f241316d368a3f98b045
DonColon <drrafshi@hotmail.de>
2019-04-04 20:22:19 +0200

    Implemented String utility class

---
+++ src/main/java/com/dardan/rrafshi/commons/Strings.java
@@ -0,0 +1,41 @@
+package com.dardan.rrafshi.commons;
+
+public final class Strings
+{
+	public static boolean isBlank(final CharSequence value)
+	{
+		if(value == null || value.length() == 0 || " ".equals(value))
+			return true;
+		else
+			return false;
+	}
+
+	public static boolean isNotBlank(final CharSequence value)
+	{
+		return !isBlank(value);
+	}
+
+	public static boolean isEmpty(final CharSequence value)
+	{
+		if(value == null || value.length() == 0)
+			return true;
+		else
+			return false;
+	}
+
+	public static boolean isNotEmpty(final CharSequence value)
+	{
+		return !isEmpty(value);
+	}
+
+	public static CharSequence removeCharacter(final String value, final String character)
+	{
+		if(value == null || character == null)
+			return null;
+
+		return String.join("", value.split(character));
+	}
+
+
+	private Strings() {}
+}
